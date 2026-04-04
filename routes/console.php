3f6f95126993d21ddac11874a80ad484cd168661
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2026-04-05 01:30:40 +0200

    Backup

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Backup scheduled tasks
+Schedule::command('backup:run')->daily()->at('00:00');
+Schedule::command('backup:clean')->daily()->at('01:00');
